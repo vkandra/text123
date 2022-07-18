3df6976ef4404ae31c274a28d47f0a17a9764e1f
Rawnak Chourasia <rc60589@corp.cyient.com>
2022-07-18 17:11:55 +0530

    Outline Sketch of elements

--- src/components/TextExtraction/TextExtraction.js
+++ src/components/TextExtraction/TextExtraction.js
@@ -3,7 +3,30 @@ import React from 'react';
 // import { connect } from 'react-redux/es/exports';
 
 const TextExtraction = (props) => {
-  return <div className="textExtraction">This is TextExtraction. Hi Hello</div>;
+  return (
+    <div className="textExtraction">
+      <div className="exactDoc">
+        <div className="dropdownsdocpage">
+          <div className="docdropdown"></div>
+          <div className="pagedropdown"></div>
+        </div>
+        <div className="displayArea"></div>
+        <div className="nextPrevButtons">
+          <div className="prevButton"></div>
+          <div className="nextButton"></div>
+        </div>
+      </div>
+
+      <div className="docData">
+        <div className="docDataAllTabs">
+          <button className="docTab1">Key-Value</button>
+          <button className="docTab1">Table</button>
+          <button className="docTab1">Raw Data</button>
+        </div>
+        <div className="docTabData"></div>
+      </div>
+    </div>
+  );
 };
 
 // const mapStateToProps = (state) => {
