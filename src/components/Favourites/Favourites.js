368c04c698c3d6abc020a49c8f595b2435ac94a0
Rawnak Chourasia <rc60589@corp.cyient.com>
2022-11-02 18:13:30 +0530

    Favourites

--- src/components/Favourites/Favourites.js
+++ src/components/Favourites/Favourites.js
@@ -1,17 +1,38 @@
 import './Favourites.css';
 import React from 'react';
-// import { connect } from 'react-redux/es/exports';
+import { connect } from 'react-redux/es/exports';
 
 const Favourites = (props) => {
-  return <div className="favourites">This is Favourites</div>;
+  return (
+    <div className="favourites">
+      <div className="favouritesText">
+        <div className="favouritesHeader">
+          <div>
+            {props.themeLang.languageWords.Template} : &nbsp;
+            <b>{props.singleDocument.singleDocumentTemplate}</b>
+          </div>
+          <div>
+            <select id="selectDataType">
+              <option>Key-Value</option>
+              <option>Table</option>
+              <option>Raw Data</option>
+            </select>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
-// const mapStateToProps = (state) => {
-//     return {
-//       user: state.user,
-//     };
-//   };
+const mapStateToProps = (state) => {
+  return {
+    user: state.user,
+    extractor: state.extractor,
+    singleDocument: state.singleDocument,
+    themeLang: state.themeLang,
+  };
+};
 
-//   export default connect(mapStateToProps)(Favourites);
+export default connect(mapStateToProps)(Favourites);
 
-export default Favourites;
+// export default Favourites;
