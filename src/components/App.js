568401ddea8baa46585e97841813f424035fb1c6
Rawnak Chourasia <rc60589@corp.cyient.com>
2023-02-21 11:37:20 +0530

    small change

--- src/components/App.js
+++ src/components/App.js
@@ -3,18 +3,8 @@ import React from 'react';
 import { connect } from 'react-redux/es/exports';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
-// import InAppBody from '../pages/InAppBody/InAppBody';
 import InAppBody from './InAppBody/InAppBody';
-// import SignIn from '../pages/SignIn/SignIn';
-// import SignUp from '../pages/SignUp.js/SignUp';
-// import Homepage from '../pages/Homepage/Homepage';
 import Homepage from './Homepage/Homepage';
-
-// import { useAuthenticator } from '@aws-amplify/ui-react';
-// import { Amplify } from 'aws-amplify';
-// import { AmpLogin } from './AmpComponents/AmpLogin';
-
-// Amplify.configure(awsExports);
 
 const App = (props) => {
   // const { user } = useAuthenticator();
